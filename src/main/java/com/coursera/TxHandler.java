cf5dc280e26ad1f767a22a94dfc472f1ec610fc7
Amit Rangra <amit.rangra@zalando.de>
2018-01-05 15:01:36 +0100

    First commit

---
+++ src/main/java/com/coursera/TxHandler.java
@@ -0,0 +1,61 @@
+package com.coursera;
+
+public class TxHandler {
+
+    private UTXOPool myUTXOPool;
+
+    /**
+     * Creates a public ledger whose current UTXOPool (collection of unspent transaction outputs) is
+     * {@code utxoPool}. This should make a copy of utxoPool by using the UTXOPool(UTXOPool uPool)
+     * constructor.
+     */
+    public TxHandler(UTXOPool utxoPool) {
+        // IMPLEMENT THIS
+        myUTXOPool = new UTXOPool(utxoPool);
+    }
+
+    /**
+     * @return true if:
+     * (1) all outputs claimed by {@code tx} are in the current UTXO pool,
+     * (2) the signatures on each input of {@code tx} are valid,
+     * (3) no UTXO is claimed multiple times by {@code tx},
+     * (4) all of {@code tx}s output values are non-negative, and
+     * (5) the sum of {@code tx}s input values is greater than or equal to the sum of its output
+     * values; and false otherwise.
+     */
+    public boolean isValidTx(Transaction tx) {
+        // IMPLEMENT THIS
+        int sumInput = 0, sumOutput = 0;
+        for (int index = 0; index < tx.numInputs(); index++) {
+            Transaction.Input input = tx.getInput(index);
+            Transaction.Output output = tx.getOutput(index);
+            UTXO utxo = new UTXO(input.prevTxHash, input.outputIndex);
+            if (myUTXOPool.contains(utxo) && Crypto.verifySignature(output.address, tx.getRawDataToSign(index) ,input.signature)
+                    && output.value >= 0) {
+
+            }
+            Transaction.Output output = myUTXOPool.getTxOutput(utxo);
+        }
+        for (Transaction.Input input : tx.getInputs()) {
+            UTXO utxo = new UTXO(input.prevTxHash, input.outputIndex);
+            if (myUTXOPool.contains(utxo)) {
+                Crypto.verifySignature(input. ,input.signature);
+                input.signature
+            }
+            Transaction.Output output = myUTXOPool.getTxOutput(utxo);
+        }
+
+        return true;
+    }
+
+    /**
+     * Handles each epoch by receiving an unordered array of proposed transactions, checking each
+     * transaction for correctness, returning a mutually valid array of accepted transactions, and
+     * updating the current UTXO pool as appropriate.
+     */
+    public Transaction[] handleTxs(Transaction[] possibleTxs) {
+        // IMPLEMENT THIS
+        return null;
+    }
+
+}
